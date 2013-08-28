68e8befb7ca4efeec341e167438291b416800c90
Michael Mayo <mmayo@waikato.ac.nz>
2013-08-28 13:04:18 +1200

    Fixed run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 javac -cp ~/Applications/weka-3-7-9/weka.jar:./ ./weka/filters/unsupervised/instance/*.java
 javac -cp ~/Applications/weka-3-7-9/weka.jar:./ ./weka/filters/supervised/instance/*.java
 
-java -cp ~/Applications/weka-3-7-9/weka.jar weka.classifiers.trees.J48 -t datasets/plethodon.arff 
-java -cp ~/Applications/weka-3-7-9/weka.jar:./ weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.instance.GPAFilter2D -S 42 -I 5 -C false" -W weka.classifiers.trees.RandomForest -t datasets/plethodon.arff -- -I 100 
-java -cp ~/Applications/weka-3-7-9/weka.jar:./ weka.classifiers.meta.FilteredClassifier -F "weka.filters.supervised.instance.SupervisedGPAFilter2D -S 42 -I 5 -C true" -W weka.classifiers.trees.RandomForest -t datasets/plethodon.arff -- -I 100 
+java -cp ~/Applications/weka-3-7-9/weka.jar weka.classifiers.trees.J48 -t datasets/plethodon-species.arff 
+java -cp ~/Applications/weka-3-7-9/weka.jar:./ weka.classifiers.meta.FilteredClassifier -F "weka.filters.unsupervised.instance.GPAFilter2D -S 42 -I 5 -C false" -W weka.classifiers.trees.RandomForest -t datasets/plethodon-species.arff -- -I 100 
+java -cp ~/Applications/weka-3-7-9/weka.jar:./ weka.classifiers.meta.FilteredClassifier -F "weka.filters.supervised.instance.SupervisedGPAFilter2D -S 42 -I 5 -C true" -W weka.classifiers.trees.RandomForest -t datasets/plethodon-species.arff -- -I 100 
